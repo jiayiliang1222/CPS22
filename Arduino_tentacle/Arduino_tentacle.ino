a26f417a534d66e8f27ee2a0ec887fe012835f84
Justine Lange <julange@gmail.com>
2023-10-23 11:39:21 +0100

    comments

--- Arduino_tentacle/Arduino_tentacle.ino
+++ Arduino_tentacle/Arduino_tentacle.ino
@@ -6,14 +6,32 @@ ServoDriver servo;
 
 int gesture = 1;
 
+//tjd: change name of variable "speed" to "tenticleSpeed"
 float speed = 0;
 
+//tjd: change angle to "tenticleRetractionAngle"
 float angle = 0;
+
+//tjd: change directinon
 int direction;
+
+
+/* tdj: create an enum to contain the gestures, so they can be referenced by name 
+
+--create datatype to hold of user emotions
+--create datatype to hold jellyfish human emotions
+--create map to specify the reactions between them; i.e. is the jellyfish just a mirror, or something more? 
+maybe it's calm when we're happy, and happy when we're calm (for instance)
+
+
+*/
 
 bool calm = false; 
 bool happy = false; 
 bool mad = false; 
+
+/* tdj create struct for servos */
+
 
 void setup() {
   Wire.begin();
@@ -61,6 +79,15 @@ void loop() {
   }else if(angle <= 0){
     direction = 1;
   }
+
+/*
+// jtd: add averaging to angle. (e.g. angle = (new angle * 0.1) + (lastAngle * 0.9) )
+
+// tjd: add clock/timing library
+// tjd: loop to go thru servos could update the order of which servo is adjusted first (randomly) whenever there is a change in emotion 
+
+ */
+
    servo.setAngle(1, angle);
    servo.setAngle(2, angle);
    servo.setAngle(3, angle);
