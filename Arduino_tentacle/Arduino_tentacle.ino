1a179b83ec9cae2061e8739ad063b2ec2d41ae49
Justine Lange <julange@gmail.com>
2023-10-23 14:06:21 +0100

    not done, but encapsulated most of the code

--- Arduino_tentacle/Arduino_tentacle.ino
+++ Arduino_tentacle/Arduino_tentacle.ino
@@ -1,13 +1,68 @@
 #include <Servo.h>
 #include "PCA9685.h"
 #include <Wire.h>
+#include <BasicTimer.h>
+  //NOTE: install Basic Timer library https://github.com/rtnate/arduino-BasicTimer
+
+
+
 
 ServoDriver servo;
+
+
+struct Appendage {
+
+  init(int _servoPin) {
+    servoPin = _servoPin;
+  }
+  
+  void setAngle(float _angle) {
+    destinationAngle = _angle;
+
+    if (destinationAngle != lastDestinationAngle){
+      destinationAngle = lastDestinationAngle;
+    }
+  }  
+
+  void update() {
+
+    if(currentAngle >= 180 || currentAngle <= 0) {  //we run this check first to make sure we don't send the servos out of bounds
+      direction = direction * -1; 
+    }
+   
+    currentAngle = (currentAngle*smoothingFactor) + (destinationAngle* (1-smoothingFactor));
+    //currentAngle = currentAngle * direction;
+    //servo.setAngle(servoPin, currentAngle);
+  }
+   
+  float smoothingFactor = 0.9; //the higher the value, the most smoothly movement and direction will change
+  float destinationAngle;
+  float lastDestinationAngle;
+  float currentAngle;
+  int direction;
+  int lastDirection;
+  float tentacleSpeed;
+  int servoPin;
+
+  // void setAngle
+  //   if (gesture>1){
+  // angle = angle + speed * direction;
+  // if (angle>=180){
+  //   direction= -1;
+  // }else if(angle <= 0){
+  //   direction = 1;
+  // }
+
+} tentacleOne,tentacleTwo, tentacleThree, tentacleFour;
+
+
+
+
 
 int gesture = 1;
 
 //tjd: change name of variable "speed" to "tenticleSpeed"
-float speed = 0;
+// float speed = 0;
 
 //tjd: change angle to "tenticleRetractionAngle"
 float angle = 0;
@@ -26,9 +81,33 @@ maybe it's calm when we're happy, and happy when we're calm (for instance)
 
 */
 
-bool calm = false; 
-bool happy = false; 
-bool mad = false; 
+const int numberOfAnimalEmotions = 5;
+
+enum Emotion { 
+
+  CALM = 1,  //not an emotion, but useful
+  HAPPY = 2,  //joyful
+  MAD = 3, //angry
+  SAD = 4,
+  AFRAID = 5 //scared
+};
+
+
+struct animal {
+
+  Emotion currentEmotion;
+  float currentSpeed;
+  float defaultSpeeds[4];
+  void setEmotion(int _emotion) {
+   currentEmotion = _emotion;
+   currentSpeed = defaultSpeeds[_emotion];
+  }
+} jellyfish;
+
+
+// bool calm = false; 
+// bool happy = false; 
+// bool mad = false; 
 
 /* tdj create struct for servos */
 
@@ -37,25 +116,50 @@ void setup() {
   Wire.begin();
   Serial.begin(9600);
   servo.init(0x7f);
+
+
+  tentacleOne.init(1);
+  tentacleTwo.init(2);
+  tentacleThree.init(3);
+  tentacleFour.init(4);
+
+
+  jellyfish.currentEmotion = CALM; //starting emotion
+  jellyfish.defaultSpeeds[CALM] = 0.05;
+  jellyfish.defaultSpeeds[HAPPY] = 0.15;
+  jellyfish.defaultSpeeds[MAD] = 0.3;
+
+
 }
 
 void loop() {
  while (Serial.available()) {
   gesture = Serial.read();
+
   }
 
+  jellyfish.setEmotion(gesture);
 
-   if (gesture == 2){
+
+  //replace these calls with our new appendage code
+   servo.setAngle(1, angle);
+   servo.setAngle(2, angle); //100 ms later run this
+   servo.setAngle(3, angle); //100 ms later run this
+   servo.setAngle(4, angle); //100 ms later run this
+
+
+/*
+   if (gesture == CALM){
     calm = true;
     happy = false;
     mad = false;
   }
-    if (gesture == 3){
+    if (gesture == HAPPY){
     happy = true;
     mad = false;
     calm = false;
   }
-    if (gesture == 4){
+    if (gesture == MAD){
     mad = true;
     happy = false;
     calm = false;
@@ -71,14 +175,16 @@ void loop() {
    if (mad){
    speed = 0.3;
   }
+
+  */  
   
-  if (gesture>1){
-  angle = angle + speed * direction;
-  if (angle>=180){
-    direction= -1;
-  }else if(angle <= 0){
-    direction = 1;
-  }
+  // if (gesture>1){
+  // angle = angle + speed * direction;
+  // if (angle>=180){
+  //   direction= -1;
+  // }else if(angle <= 0){
+  //   direction = 1;
+  // }
 
 /*
 // jtd: add averaging to angle. (e.g. angle = (new angle * 0.1) + (lastAngle * 0.9) )
@@ -88,10 +194,10 @@ void loop() {
 
  */
 
-   servo.setAngle(1, angle);
-   servo.setAngle(2, angle);
-   servo.setAngle(3, angle);
-   servo.setAngle(4, angle);
+
+
+
+
 
 
   }
@@ -102,4 +208,4 @@ void loop() {
 
 
 
-}
+
